95aa4f648a55884d6811f1a77fe5f7d73f6f57c4
Lincong <2997534654@qq.com>
2023-10-22 08:04:02 +0800

    [Feat]: 完成k8s job pod的搭建

--- hdfs-cluster/cluster/ssh-login.sh
+++ hdfs-cluster/cluster/ssh-login.sh
@@ -3,4 +3,5 @@ sudo ssh-copy-id -i /root/.ssh/id_rsa -p 22 root@172.16.238.10
 sudo ssh-copy-id -i /root/.ssh/id_rsa -p 22 root@172.16.238.11
 sudo ssh-copy-id -i /root/.ssh/id_rsa -p 22 root@172.16.238.12
 
+hdfs namenode -format
 bash start-all.sh
